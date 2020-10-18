5ee3e3bbcc7c10263f647ab3d9c9d13df669b894
Thomas Inman <68563083+Thomas-Inman@users.noreply.github.com>
2020-10-17 21:59:14 -0400

    Update my_custom_commands.sh

--- my_custom_commands.sh
+++ my_custom_commands.sh
@@ -6,20 +6,8 @@ function nfe() {
     pwd | pbcopy
     cd ~
     # Replace the path below with the path to the nfe_command.py script
-    cd /Users/thomasinman/coding/NotesAutomation
+    cd # Your PATH
     python3 nfe_command.py $1 $2 $3
-    cd ~
-    cd $(pbpaste)
-}
-
-
-# TicTacToe game function
-function TicTacToe() {
-    pwd | pbcopy
-    cd ~
-    # Replace the path below with the path to the TicTacToe.py script
-    cd /Users/thomasinman/coding/TicTacToe
-    python3 TicTacToe2.py
     cd ~
     cd $(pbpaste)
 }
@@ -30,7 +18,7 @@ function podcast() {
     pwd | pbcopy
     cd ~
     # Replace the path below with the path to the PodcastFunction.py script
-    cd /Users/thomasinman/coding/PodcastAutomation
+    cd # Your PATH
     python3 PodcastFunction.py $1
     cd ~
     cd $(pbpaste)
